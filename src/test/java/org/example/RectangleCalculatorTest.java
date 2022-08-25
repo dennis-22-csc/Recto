3ce81142b9f39821076abe42036a50ba298d1b97
dennis-22-csc <108535077+dennis-22-csc@users.noreply.github.com>
2022-08-25 16:08:13 +0100

    Routine refactoring

--- src/test/java/org/example/RectangleCalculatorTest.java
+++ src/test/java/org/example/RectangleCalculatorTest.java
@@ -4,11 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
